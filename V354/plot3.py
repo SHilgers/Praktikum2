260b2db80c699740ba9493ce378182b7bb0d16e3
„Lara <lara.nollen@tu-dortmund.de>
2018-01-07 11:01:33 +0100

    Mein ganzes Zeug

---
+++ V354/plot3.py
@@ -0,0 +1,24 @@
+import numpy as np
+from scipy.optimize import curve_fit
+import matplotlib.pyplot as plt
+from uncertainties import ufloat
+
+a, b, c= np.genfromtxt('Werte6.txt', unpack=True)
+
+d= b/c
+
+f=4.479/np.sqrt(2)
+#plt.plot(t, f(t, *params), 'b-' ,label='Lineare Regression')
+
+plt.plot (a, d, 'rx', label='Messwerte')
+#plt.plot(x,f(x, *params), 'b-' ,label='Ausgleichsfunktion f(x)')
+plt.axhline(y=3.167131, color='slateblue', linestyle='--')
+plt.axvline(x=29000, color='slateblue', linestyle='--')
+plt.axvline(x=37500 , color='slateblue', linestyle='--')
+plt.grid()
+plt.xlabel(r'$\nu / hz$')
+plt.ylabel(r'$ \frac{U_c}{U}$')
+plt.legend(loc="best")
+plt.tight_layout()
+plt.savefig('plot3.pdf')
+plt.show()
