b5a7a9ce64dc6b9667595f270364827474ae1e1b
Stefanie Hilgers <hilgers-stefanie@web.de>
2018-01-10 07:18:34 +0100

    m

--- V308/Helmholz1I.py
+++ V308/Helmholz1I.py
@@ -4,16 +4,16 @@ import matplotlib.pyplot as plt
 from uncertainties import ufloat
 
 x, y = np.genfromtxt('Helmholz1I.txt', unpack=True)
-x=x/1000
+x=x/100
 def g(x):
-    return ((4*np.pi*10**(-7)*2*0.0625**2)/(0.0625**2+ (x+0.0312)**2)**(3/2))*200*1000
+    return ((4*np.pi*10**(-7)*2*0.0625**2)/(0.0625**2+ (x+0.0312)**2)**(3/2))*200*100
 
-t= np.linspace(0.0008 , 0.0018, 1000)
+t= np.linspace(0.008 , 0.018, 1000)
 
 plt.plot (x,y, 'rx', label='Messwerte')
 plt.plot(t,g(t) ,'-b', label='Theoriekurve')
 #plt.plot(t, f(t, *params), 'b-' ,label='Lineare Regression')
-plt.xlabel(r'Abstand von der Spule 1 in der Einheit $\mathrm{10m}$')
+plt.xlabel(r'Abstand von der Spule 1 in m')
 plt.ylabel(r'B/ mT')
 plt.legend()
 #plt.show()
--- V308/Helmholz2I.py
+++ V308/Helmholz2I.py
@@ -4,17 +4,17 @@ import matplotlib.pyplot as plt
 from uncertainties import ufloat
 
 x, y = np.genfromtxt('Helmholz2I.txt', unpack=True)
-x=x/1000
+x=x/100
 
 def g(x):
-    return ((5*4*np.pi*10**(-7)*0.0625**2)/(0.0625**2+ (x+0.00312)**2)**(3/2))*200*1000
+    return ((5*4*np.pi*10**(-7)*0.0625**2)/(0.0625**2+ (x+0.00312)**2)**(3/2))*200*100
 
-t= np.linspace(0.0007, 0.00175, 1000)
+t= np.linspace(0.007, 0.0175, 1000)
 
 plt.plot (x,y, 'rx', label='Messwerte')
 plt.plot(t, g(t), 'b-', label='Theoriekurve')
 #plt.plot(t, f(t, *params), 'b-' ,label='Lineare Regression')
-plt.xlabel(r'Abstand von der Spule 1 in der Einheit 10m')
+plt.xlabel(r'Abstand von der Spule 1 in der Einheit m')
 plt.ylabel(r'B/ mT')
 plt.legend()
 #plt.show()
